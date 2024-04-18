8867e9e52f1d811f862ebd635bb020da0c8f36d3
thedunncodes <tobioluwa348@gmail.com>
2024-04-18 22:25:11 +0000

    Basic Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -65,5 +65,8 @@ if __name__ == "__main__":
     if getenv('AUTH_TYPE') == 'auth':
         from api.v1.auth.auth import Auth
         auth = Auth()
+    if getenv('AUTH_TYPE') == 'basic_auth':
+        from api.v1.auth.basic_auth import BasicAuth
+        auth = BasicAuth()
 
     app.run(host=host, port=port)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" BasicAuth module """
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ A BasicAuth class that inherits same
+    functionalities from a class Auth
+    """
+    pass
