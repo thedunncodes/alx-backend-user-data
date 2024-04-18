184e4cb4e26467334ff0fa09da5808527dd681d1
thedunncodes <tobioluwa348@gmail.com>
2024-04-18 22:40:09 +0000

    Basic Auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     if getenv('AUTH_TYPE') == 'auth':
         from api.v1.auth.auth import Auth
         auth = Auth()
-    if getenv('AUTH_TYPE') == 'basic_auth':
+    elif getenv('AUTH_TYPE') == 'basic_auth':
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
 
