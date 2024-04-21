58e09dae30da9df0d51ff6cd4f8ff772016566ea
thedunncodes <tobioluwa348@gmail.com>
2024-04-21 22:46:51 +0000

    Empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" SessionAuth module """
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ A SessionAuth class that inherits same
+    functionalities from a class Auth and
+    also provides session auth type functions
+    """
+
+    pass
