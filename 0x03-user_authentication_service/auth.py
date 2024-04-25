9e408b5cd14a1d575870f20f6ed733f01b5cd22d
thedunncodes <tobioluwa348@gmail.com>
2024-04-25 19:20:15 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
+"""A module for authentication-related routines.
 """
-Auth module
-"""
-
 import bcrypt
+from uuid import uuid4
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+
 from db import DB
 from user import User
-from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
-    """ Hashes A password
-    with Bcrypt
+    """Hashes a password.
     """
-    hashed_pw = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
 
-    return hashed_pw
+
+def _generate_uuid() -> str:
+    """Generates a UUID.
+    """
+    return str(uuid4())
 
 
 class Auth:
@@ -23,17 +27,18 @@ class Auth:
     """
 
     def __init__(self):
+        """Initializes a new Auth instance.
+        """
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """ Registers a new user
+        """Adds a new user to the database.
         """
         try:
-            user = self._db.find_user_by(email=email)
+            self._db.find_user_by(email=email)
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        if user:
-            raise ValueError("User {} already exists.".format(user.email))
+        raise ValueError("User {} already exists".format(email))
 
     def valid_login(self, email: str, password: str) -> bool:
         """Checks if a user's login details are valid.
